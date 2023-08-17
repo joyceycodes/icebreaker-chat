b1aee735a9cea157824622ad2796fb061f621612
kleenkanteen <antpalmolive@gmail.com>
2023-08-17 17:15:50 -0400

    changed column name to proper one

--- site/prisma/migrations/20230817211453_init/migration.sql
+++ site/prisma/migrations/20230817211453_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "rooms" (
     "room_id" SERIAL NOT NULL,
-    "question_num" INTEGER NOT NULL DEFAULT 0,
+    "current_question" INTEGER NOT NULL DEFAULT 0,
 
     CONSTRAINT "rooms_pkey" PRIMARY KEY ("room_id")
 );
