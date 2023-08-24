72b3fe9842708134dd36881cdbf86bbc1eb75fb4
Sabih S <67878058+kleenkanteen@users.noreply.github.com>
2023-08-24 00:28:08 -0400

    Create room backend functionality (#19)
    * create room (/api/room) and get room (/api/room/[id]) endpoint created.
    
    * global prisma client also added

---
+++ site/src/app/api/room/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const new_room = await prisma.rooms.create({
+        data: {
+            current_question: 0
+        }
+    })
+
+    return NextResponse.json({
+        room_id: new_room.room_id
+    });
+  } catch (error) {
+    console.error(`Error creating room: `, error);
+    return new NextResponse("Error creating room", { status: 500 });
+  }
+}
